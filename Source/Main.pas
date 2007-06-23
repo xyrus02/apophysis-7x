79132064795b6c5fb4f8eb1d77f57f0df85e8536
utak3r <utak3r>
2007-06-23 19:32:48 +0000

    Output file path corrected while exporting a flame to flam3.

--- Source/Main.pas
+++ Source/Main.pas
@@ -3961,7 +3961,7 @@ begin
         FileList.Add('set transparency=1')
       else
         FileList.Add('set transparency=0');
-      FileList.Add('set out=' + ExportDialog.Filename);
+      FileList.Add('set out="' + ExportDialog.Filename + '"');
       FileList.Add('@echo Rendering "' + ExportDialog.Filename + '"');
 {
       FileList.Add(ExtractShortPathName(hqiPath) + ' < ' + ExtractShortPathName(ChangeFileExt(ExportDialog.Filename, '.flame')));
